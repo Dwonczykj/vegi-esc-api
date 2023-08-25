abbcb58fb5a4f8da8618cf4bb4c3dc5e12231528
Dwonczykj <jdwonczyk@gmail.com>
2023-08-25 12:34:28 +0100

    heroku release now working using makeRun.sh -> docker tag -> docker push -> heroku container:release web

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,18 @@ export FLASK_APP=vegi_esc_api.app:gunicorn_app
 export FLASK_ENV=production  # Set the desired Flask environment (e.g., "development", "production")
 
 echo "Entrypoint for app run with HOME=\"$HOME\""
-echo "Copying \"/.chromadb\" to \"$HOME/.chromadb\""
-cp /.chromadb $HOME/.chromadb -r
-echo "Copying \"/env\" to \"$HOME/env\""
-cp /env $HOME/env -r
+# echo "Copying \"/.chromadb\" to \"$HOME/.chromadb\""
+# cp /.chromadb $HOME/.chromadb -r
+# echo "Copying \"/env\" to \"$HOME/env\""
+# cp /env $HOME/env -r
 
-# ~ https://stackoverflow.com/a/46933153
-export PATH="$PATH:/env/bin:$HOME/env/bin"
-# source activate $HOME/env
-export PATH="$HOME/python/bin:$PATH"
+# # ~ https://stackoverflow.com/a/46933153
+# export PATH="$PATH:/env/bin:$HOME/env/bin"
+# export PATH="$HOME/python/bin:$PATH"
+
+# Activate the virtual environment not necessary when running gunicorn from within the environment as below using /env/bin/gunicorn
+# source /env/bin/activate
+
 
 
 # Start Gunicorn
@@ -23,5 +26,5 @@ export PATH="$HOME/python/bin:$PATH"
 # exec /env/bin/gunicorn --bind 0.0.0.0:$PORT --workers 1 --timeout 600 vegi_esc_api.app:gunicorn_app
 # ! heroku web local will always fail from within mac as the
 # * use heroku run web to launch the web dyno ~ https://devcenter.heroku.com/articles/dynos#cli-commands-for-dyno-management
-exec gunicorn --bind 0.0.0.0:$PORT --workers 1 --timeout 600 vegi_esc_api.app:gunicorn_app
+exec /env/bin/gunicorn --bind 0.0.0.0:$PORT --workers 1 --timeout 600 vegi_esc_api.app:gunicorn_app
 
